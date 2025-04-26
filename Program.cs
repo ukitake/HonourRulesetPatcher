403763c504b09072f6779120ae88bf40b5b226e8
Sam Mason <sammason3388@gmail.com>
2025-04-26 01:29:22 -0500

    fixing the patcher to work with BG3 Patch 8

--- Program.cs
+++ Program.cs
@@ -8,6 +8,7 @@ Console.WriteLine("https://github.com/ukitake");
 Console.WriteLine("---------------------------------\n");
 if (args.Length == 0)
 {
+
     Console.WriteLine("ERROR: No save path provided. Full save folder path should be provided as 1st argument.\nPress any key to exit...");
     Console.ReadKey();
     Environment.Exit(-1);
@@ -99,27 +100,27 @@ var makeHonourNode = () =>
     var uuidAtt = new XElement("attribute");
     uuidAtt.SetAttributeValue("id", "UUID");
     uuidAtt.SetAttributeValue("type", "guid");
-    uuidAtt.SetAttributeValue("value", "b77b6210-ac50-4cb1-a3d5-5702fb9c744c");
+    uuidAtt.SetAttributeValue("value", "767d0062-d82c-279c-e16b-dfee7fe94cdd");
 
     var version64Att = new XElement("attribute");
     version64Att.SetAttributeValue("id", "Version64");
     version64Att.SetAttributeValue("type", "int64");
-    version64Att.SetAttributeValue("value", "36028797024738184");
+    version64Att.SetAttributeValue("value", "36028797025713249");
 
     var md5Att = new XElement("attribute");
     md5Att.SetAttributeValue("id", "MD5");
     md5Att.SetAttributeValue("type", "LSString");
-    md5Att.SetAttributeValue("value", "00da1a1b7578924324da3893ab786f32");
+    md5Att.SetAttributeValue("value", "a7986aa127818dab105e831b095419ef");
 
     var folderAtt = new XElement("attribute");
     folderAtt.SetAttributeValue("id", "Folder");
     folderAtt.SetAttributeValue("type", "LSString");
-    folderAtt.SetAttributeValue("value", "Honour");
+    folderAtt.SetAttributeValue("value", "HonourX");
 
     var nameAtt = new XElement("attribute");
     nameAtt.SetAttributeValue("id", "Name");
     nameAtt.SetAttributeValue("type", "LSString");
-    nameAtt.SetAttributeValue("value", "Honour");
+    nameAtt.SetAttributeValue("value", "HonourX");
 
     var publishAtt = new XElement("attribute");
     publishAtt.SetAttributeValue("id", "PublishHandle");
@@ -136,10 +137,51 @@ var makeHonourNode = () =>
     return honourNode;
 };
 
+var makeSingleSaveCustomRulesetValue = () =>
+{
+    var sscrv = new XElement("node");
+    sscrv.SetAttributeValue("id", "Object");
+
+    var mapKeyAtt = new XElement("attribute");
+    mapKeyAtt.SetAttributeValue("id", "MapKey");
+    mapKeyAtt.SetAttributeValue("type", "guid");
+    mapKeyAtt.SetAttributeValue("value", "338450d9-d77d-4950-9e1e-0e7f12210bb3");
+
+    var children1 = new XElement("children");
+
+    var customRulesetValue = new XElement("node");
+    customRulesetValue.SetAttributeValue("id", "CustomRulesetValue");
+
+    var children2 = new XElement("children");
+
+    var modifier = new XElement("node");
+    modifier.SetAttributeValue("id", "Modifier");
+
+    var typeAtt = new XElement("attribute");
+    typeAtt.SetAttributeValue("id", "Type");
+    typeAtt.SetAttributeValue("type", "int32");
+    typeAtt.SetAttributeValue("value", "4");
+
+    var valueAtt = new XElement("attribute");
+    valueAtt.SetAttributeValue("id", "Value");
+    valueAtt.SetAttributeValue("type", "bool");
+    valueAtt.SetAttributeValue("value", "False");
+
+    sscrv.Add(mapKeyAtt);
+    sscrv.Add(children1);
+    children1.Add(customRulesetValue);
+    customRulesetValue.Add(children2);
+    children2.Add(modifier);
+    modifier.Add(typeAtt);
+    modifier.Add(valueAtt);
+
+    return sscrv;
+};
+
 // patch the text files
 XDocument meta = XDocument.Load($"{tempSavePath}\\meta.lsx");
 // find the GustavDev node and add the Honour Mode mod right after it
-var gustavDev = meta.DescendantNodes().Where(e => (e as XElement)?.Attribute("value")?.Value == "GustavDev").FirstOrDefault()?.Parent;
+var gustavDev = meta.DescendantNodes().Where(e => (e as XElement)?.Attribute("value")?.Value == "GustavX").FirstOrDefault()?.Parent;
 if (gustavDev == null)
 {
     Error("ERROR: No 'GustavDev' <node> found in meta.lsx file.\nPress any key to exit...");
@@ -155,10 +197,54 @@ if (ruleSet == null)
     return;
 }
 (ruleSet as XElement)?.SetAttributeValue("value", "5d595a7a-6182-4559-b404-ed7cc7ad8ada");
+
+var customRulesetValues = meta.DescendantNodes().Where(e => (e as XElement).Attribute("id")?.Value == "CustomRulesetValues").FirstOrDefault() as XElement;
+if (customRulesetValues == null)
+{
+    // there is no CustomRulesetValues node so we need to add it ourselves
+    var crvs = new XElement("node");
+    crvs.SetAttributeValue("id", "CustomRulesetValues");
+    var children = new XElement("children");
+    children.Add(makeSingleSaveCustomRulesetValue());
+    crvs.Add(children);
+    (ruleSet as XElement)?.AddAfterSelf(crvs);
+} 
+else
+{
+    // find the ancestor of the single save attribute (identified by the GUID in the Where here)
+    var singleSaveAttAncestor = meta.DescendantNodes().Where(e => (e as XElement)?.Attribute("value")?.Value == "338450d9-d77d-4950-9e1e-0e7f12210bb3").FirstOrDefault();
+    if (singleSaveAttAncestor == null)
+    {
+        // single save rule value doesn't exist so add it
+        if (customRulesetValues.FirstNode == null)
+        {
+            // the CustomRulesetValues node is empty, so add the children node and add the single save value to the children
+            var children = new XElement("children");
+            children.Add(makeSingleSaveCustomRulesetValue());
+            customRulesetValues.Add(children);
+        }
+        else
+        {
+            // the CustomRulesetValues node already has children, so add the new single save value to that children node
+            (customRulesetValues.FirstNode as XElement).Add(makeSingleSaveCustomRulesetValue());
+        }
+    }
+    else
+    {
+        // there is already a single save custom ruleset value, find it in the structure and set it to False
+        var children = (singleSaveAttAncestor as XElement)?.NextNode as XElement;
+        var crv = children?.FirstNode as XElement;
+        var children2 = crv?.FirstNode as XElement;
+        var modifier = children2?.FirstNode as XElement;
+        var valueAtt = modifier?.DescendantNodes().Where(e => (e as XElement)?.Attribute("id")?.Value == "Value").FirstOrDefault() as XElement;
+        valueAtt?.SetAttributeValue("value", "False");
+    }
+}
+
 meta.Save($"{tempSavePath}\\meta.lsx");
 
 XDocument globals = XDocument.Load($"{tempSavePath}\\Globals.lsx");
-var globalsGustavDev = globals.DescendantNodes().Where(e => (e as XElement)?.Attribute("value")?.Value == "GustavDev").FirstOrDefault()?.Parent;
+var globalsGustavDev = globals.DescendantNodes().Where(e => (e as XElement)?.Attribute("value")?.Value == "GustavX").FirstOrDefault()?.Parent;
 if (globalsGustavDev == null)
 {
     Error("ERROR: No 'GustavDev' <node> found in Globals.lsx file.\nPress any key to exit...");
